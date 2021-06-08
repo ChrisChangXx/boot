c2199729d9f1db175e05f03bc5b6354707b8e9c5
zhanghui <zhanghui@chargertech.cn>
2021-06-08 17:47:40 +0800

    boot+websocket

---
+++ src/main/java/com/chris/web/Test.java
@@ -0,0 +1,11 @@
+package com.chris.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test {
+    public static void main(String[] args) {
+        SpringApplication.run(Test.class);
+    }
+}
